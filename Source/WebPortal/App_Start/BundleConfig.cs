14df36958be2da6120abededab130e18aca7fded
Matthew Thomas <malex@orbiter.me>
2014-12-20 14:48:55 -0800

    Modifies the RedisCache page to render the Redis Functions in an accordian style to reduce the amount of clutter on the page.

--- Source/WebPortal/App_Start/BundleConfig.cs
+++ Source/WebPortal/App_Start/BundleConfig.cs
@@ -11,7 +11,11 @@ namespace WebPortal
         RegisterBundles(
             BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js", "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js", 
+                "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                "~/Scripts/jquery-ui-{version}.js")
+            );
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
