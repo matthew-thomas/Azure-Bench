95c2e04cb6f846180b5c67bcac03ec71346f0b79
Matthew Thomas <malex@orbiter.me>
2014-12-20 13:32:19 -0800

    Separated StringSet and StringGet operations to better model the Azure Redis Cache functionality on the UI.

--- Source/WebPortal/Controllers/RedisCacheController.cs
+++ Source/WebPortal/Controllers/RedisCacheController.cs
@@ -13,16 +13,19 @@ namespace WebPortal.Controllers
         ActionResult 
         Index()
         {
-            var defaultOptions = new RedisBenchView {
-                ServiceSettings = new RedisServiceSettings {
+            var defaultOptions = new RedisBenchViewModel {
+                ServiceSettings = new RedisServiceSettingsViewModel {
                     ServiceName = "your-service-name-here",
                     ServiceKey  = "your-service-key-here"
                 },
-                StringSetParameters = new StringSetParameters {
+                StringSetParameters = new StringSetViewModel {
                     Key   = "Example-Key",
                     Value = "Example-Value"
                 },
-                CallSettings = new CallSettings {
+                StringGetParameters = new StringGetViewModel {
+                    Key = "Example-Key"
+                },
+                CallSettings = new CallSettingsViewModel {
                     NumberOfRepititions      = 1,
                     MaxConcurrentSubmissions = 1,
                 }
@@ -34,33 +37,80 @@ namespace WebPortal.Controllers
         public
         string
         StringSet(
-            RedisBenchView options)
+            RedisServiceSettingsViewModel   serviceSettings,
+            StringSetViewModel              stringSetParameters,
+            CallSettingsViewModel           callSettings)
         {
             try
             {
                 var redis =
-                    ConnectionMultiplexer.Connect(options.ServiceSettings.ServiceName + ".redis.cache.windows.net,password=" +
-                                                  options.ServiceSettings.ServiceKey);
+                    ConnectionMultiplexer.Connect(serviceSettings.ServiceName + ".redis.cache.windows.net,password=" +
+                                                  serviceSettings.ServiceKey);
                 var redisDb = redis.GetDatabase();
 
                 var totalStopWatch = Stopwatch.StartNew();
 
                 Parallel.For(
                     fromInclusive:      0,
-                    toExclusive:        options.CallSettings.NumberOfRepititions,
+                    toExclusive:        callSettings.NumberOfRepititions,
                     parallelOptions:    new ParallelOptions {
-                                            MaxDegreeOfParallelism = options.CallSettings.MaxConcurrentSubmissions
+                                            MaxDegreeOfParallelism = callSettings.MaxConcurrentSubmissions
                                         },
-                    body:               i => redisDb.StringSet(options.StringSetParameters.Key, options.StringSetParameters.Value)                                        
+                    body:               i => redisDb.StringSet(stringSetParameters.Key, stringSetParameters.Value)                                        
                 );
 
                 var totalElapsedMilliseconds    = totalStopWatch.ElapsedMilliseconds;
-                var documentsPerSecond          = options.CallSettings.NumberOfRepititions / (totalElapsedMilliseconds / 1000.0);
-                var averageLatencyMilliseconds  = (double)totalElapsedMilliseconds / options.CallSettings.NumberOfRepititions;
+                var documentsPerSecond          = callSettings.NumberOfRepititions / (totalElapsedMilliseconds / 1000.0);
+                var averageLatencyMilliseconds  = (double)totalElapsedMilliseconds / callSettings.NumberOfRepititions;
 
                 return string.Format(
-                    "Set {0} cache items in {1}ms\r\nCalculated Rates: {2}/sec @ {3}ms/set avg",
-                    options.CallSettings.NumberOfRepititions,
+                    "Set {0} items in {1}ms\r\nRate: {2}/sec @ {3}ms/set avg",
+                    callSettings.NumberOfRepititions,
+                    totalElapsedMilliseconds,
+                    documentsPerSecond.ToString("0.00"),
+                    averageLatencyMilliseconds
+                );
+
+            }
+            catch (Exception exception)
+            {
+                return exception.ToString();
+            }
+        }
+
+        public
+        string
+        StringGet(
+            RedisServiceSettingsViewModel   serviceSettings,
+            StringGetViewModel              stringGetParameters,
+            CallSettingsViewModel           callSettings)
+        {
+            try
+            {
+                var redis =
+                    ConnectionMultiplexer.Connect(serviceSettings.ServiceName + ".redis.cache.windows.net,password=" +
+                                                  serviceSettings.ServiceKey);
+                var redisDb = redis.GetDatabase();
+
+                var totalStopWatch = Stopwatch.StartNew();
+
+                Parallel.For(
+                    fromInclusive: 0,
+                    toExclusive: callSettings.NumberOfRepititions,
+                    parallelOptions: new ParallelOptions
+                    {
+                        MaxDegreeOfParallelism = callSettings.MaxConcurrentSubmissions
+                    },
+                    body: i => redisDb.StringGet(stringGetParameters.Key)
+                );
+
+                var totalElapsedMilliseconds = totalStopWatch.ElapsedMilliseconds;
+                var documentsPerSecond = callSettings.NumberOfRepititions / (totalElapsedMilliseconds / 1000.0);
+                var averageLatencyMilliseconds = (double)totalElapsedMilliseconds / callSettings.NumberOfRepititions;
+
+                return string.Format(
+                    "Get {0} items in {1}ms\r\nRate: {2}/sec @ {3}ms/set avg",
+                    callSettings.NumberOfRepititions,
                     totalElapsedMilliseconds,
                     documentsPerSecond.ToString("0.00"),
                     averageLatencyMilliseconds
@@ -74,26 +124,32 @@ namespace WebPortal.Controllers
         }
     }
 
-    public class RedisBenchView
+    public class RedisBenchViewModel
     {
-        [DisplayName("Service Settings")]   public RedisServiceSettings ServiceSettings     { get; set; }
-        [DisplayName("StringSet Settings")] public StringSetParameters  StringSetParameters { get; set; }
-        [DisplayName("Call Settings")]      public CallSettings         CallSettings        { get; set; }
+        [DisplayName("Service Settings")]   public RedisServiceSettingsViewModel    ServiceSettings     { get; set; }
+        [DisplayName("StringSet Settings")] public StringSetViewModel               StringSetParameters { get; set; }
+        [DisplayName("StringGet Settings")] public StringGetViewModel               StringGetParameters { get; set; }
+        [DisplayName("Call Settings")]      public CallSettingsViewModel            CallSettings        { get; set; }
     }
 
-    public class RedisServiceSettings
+    public class RedisServiceSettingsViewModel
     {
         public string ServiceName   { get; set; }
         public string ServiceKey    { get; set; }
     }
 
-    public class StringSetParameters
+    public class StringSetViewModel
     {
         public string Key              { get; set; }
         public string Value            { get; set; }
     }
 
-    public class CallSettings
+    public class StringGetViewModel
+    {
+        public string Key { get; set; }
+    }
+
+    public class CallSettingsViewModel
     {
         public int NumberOfRepititions      { get; set; }
         public int MaxConcurrentSubmissions { get; set; }        
