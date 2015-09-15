001731b8a361aca01cc49de579db222a9d44bdaa
Matthew Thomas <malex@orbiter.me>
2015-09-15 11:21:00 -0700

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,9 +1,5 @@
-Write-Host $env:PATH
-
 # Install chocolately if it's not already installed
 if (-not ($env:Path -ilike "*chocolatey*")) {
     Write-Host "Chocolatey not found in PATH environment variable, installing Chocolatey..."
-    #iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-} else {
-    Write-Host "CONTAINS!"
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 }
