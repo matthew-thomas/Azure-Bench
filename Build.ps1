621575df70753b0d4608198e3a841c969c964efe
Matthew Thomas <malex@orbiter.me>
2015-09-15 11:50:18 -0700

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,12 @@
 # Install chocolately if it's not already installed
 if (-not ($env:Path -ilike "*chocolatey*")) {
-    Write-Host "Chocolatey not found in PATH environment variable, installing Chocolatey..."
+    Write-Host "Chocolatey not found in PATH environment variable, installing..."
+    
     iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 }
+
+# Make sure all other required dependencies are installed...
+cinst nuget.commandline --version 2.8.6 --confirm
+
+# Make sure all NuGet packages are installed...
+nuget restore .\Source
